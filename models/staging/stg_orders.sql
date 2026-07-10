38039e3bfd9455f13e749243089ac038d59e5446
Omar Qatab <iamomar2624@gmail.com>
2026-07-10 00:47:52 +0000

    installed dbt packages generate_surrogate_key

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,25 +1,24 @@
-select 
---raw orders
-o.orderid,
-o.orderdate,
-o.shipdate,
-o.shipmode,
-o.ordersellingprice - ordercostprice as orderprofit,
-o.ordersellingprice,
-ordercostprice,
---raw customer
-c.customerid,
-c.customername,
-c.segment,
-c.country,
---raw product
-p.category,
-p.productname,
-p.subcategory,
-p.productid
-from {{ ref('raw_orders') }} as o
-left join {{ ref('raw_customer') }} as c
-on o.customerid = c.customerid
-left join {{ ref('raw_product') }} as p
-on o.productid = p.productid
-
+select
+    -- raw orders
+    {{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }}
+    as sk_orders,
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordersellingprice - ordercostprice as orderprofit,
+    o.ordersellingprice,
+    ordercostprice,
+    -- raw customer
+    c.customerid,
+    c.customername,
+    c.segment,
+    c.country,
+    -- raw product
+    p.category,
+    p.productname,
+    p.subcategory,
+    p.productid
+from {{ ref("raw_orders") }} as o
+left join {{ ref("raw_customer") }} as c on o.customerid = c.customerid
+left join {{ ref("raw_product") }} as p on o.productid = p.productid
