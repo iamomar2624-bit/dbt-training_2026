b3ca2d3dbb1982cbb4eb2b28249a3a91b1ef3f29
Omar Qatab <iamomar2624@gmail.com>
2026-07-10 01:26:25 +0000

    added delivery_team seed and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -18,7 +18,9 @@ select
     p.category,
     p.productname,
     p.subcategory,
-    p.productid
+    p.productid,
+    d.delivery_team
 from {{ ref("raw_orders") }} as o
 left join {{ ref("raw_customer") }} as c on o.customerid = c.customerid
 left join {{ ref("raw_product") }} as p on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d on o.shipmode = d.shipmode
